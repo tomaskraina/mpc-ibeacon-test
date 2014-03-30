99c33bbe8d0a3630623afe13f84e8b5d7f289be8
Tom K <krajeec@gmail.com>
2014-03-30 23:15:31 +0200

    Initial Commit

---
+++ MPCTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MPCTest
+//
+//  Created by Tom K on 3/30/14.
+//  Copyright (c) 2014 Tom Kraina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MPCTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MPCTest
+//
+//  Created by Tom K on 3/30/14.
+//  Copyright (c) 2014 Tom Kraina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
